a7524affd05157de9def032af7a82544d7dd1668
Dmytro Kyrychenko <dmytro.kyrychenko@plus4u.net>
2024-03-13 00:37:07 +0200

    app

---
+++ src/app/providers.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { NextUIProvider } from "@nextui-org/react";
+import { SessionProvider } from "next-auth/react";
+
+interface Providers {
+  children: React.ReactNode;
+}
+
+export default function Providers({ children }: Providers) {
+  return (
+    <SessionProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </SessionProvider>
+  );
+}
